098c27a6f0e751a8285310a97d17696d2b67e579
liangliangliu <710168912@qq.com>
2015-07-09 11:37:49 +0800

    再次测试pull

--- app/src/main/java/com/getui/app/App.java
+++ app/src/main/java/com/getui/app/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "Hello World ni hao wo hao de hahha   zuixin!" );
         System.out.println( "wo de xin zai yi ci  " ); 
         System.out.println( "mimgtian hui geng hao" ); 
+        System.out.println( "test pull" ); 
     }
 }
